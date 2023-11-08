e8bed13fa4e0b2c92b11db01c58745711d376b22
shofiq <shofiqebr@gmail.com>
2023-11-08 18:12:54 +0600

    added modal

--- src/pages/SingleService.jsx
+++ src/pages/SingleService.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
 const SingleService = () => {
-    const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const service = useLoaderData();
   const {
     _id,
@@ -18,30 +18,77 @@ const SingleService = () => {
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content  flex-col lg:flex-row-reverse">
-
           <div className="w-[300px]">
             <div>Service provider</div>
-          <img
-            src={user.photoURL}
-            className="w-[300px] rounded-lg shadow-2xl"
-          />
-          <div>
-            {user.displayName} <br />
-            {serviceArea}
-
-          </div>
-
+            <img
+              src={user.photoURL}
+              className="w-[300px] rounded-lg shadow-2xl"
+            />
+            <div>
+              {user.displayName} <br />
+              {serviceArea}
+            </div>
           </div>
           <div>
-    <img className="h-64 w-80" src={serviceImage} alt="" />
+            <img className="h-64 w-80" src={serviceImage} alt="" />
             <h1 className="text-5xl font-bold">{serviceName}</h1>
-            <p className="">
-              {serviceDescription}
-            </p>
-            <p className="py-6">
-              {servicePrice}
-            </p>
-            <button className="btn btn-primary">Book Now</button>
+            <p className="">{serviceDescription}</p>
+            <p className="py-6">{servicePrice}</p>
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <button
+              className="btn"
+              onClick={() => document.getElementById("my_modal_1").showModal()}
+            >
+              Book Now
+            </button>
+            <dialog id="my_modal_1" className="modal">
+              <div className="modal-box">
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  value={serviceName}
+                  readOnly
+                />
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  value={serviceImage}
+                  readOnly
+                />
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  value={user.email}
+                  readOnly
+                />
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  placeholder="service taking date"
+                  
+                />
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  placeholder="special instruction"
+                />
+                <input
+                  className="input input-bordered"
+                  type="text"
+                  value={servicePrice}
+                  readOnly
+                />
+               <button className="btn">purchase now</button>
+               
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
           </div>
         </div>
       </div>
