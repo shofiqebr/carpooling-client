c21f51739a2aeda116d9fcd61adca06778e076c5
shofiq <shofiqebr@gmail.com>
2023-11-09 12:40:53 +0600

    added update and delete card

--- src/provider/components/ManageServiceCard.jsx
+++ src/provider/components/ManageServiceCard.jsx
@@ -1,16 +1,37 @@
-
 // import { useContext } from 'react';
 
 // import { AuthContext } from '../AuthProvider';
 
-const ManageServiceCard = ({service}) => {
-    const { _id, serviceImage, serviceName, serviceDescription,servicePrice, } = service;
-    // const {user} = useContext(AuthContext);
-    return (
-        <div>
-            
+const ManageServiceCard = ({ service }) => {
+  const { _id, serviceImage, serviceName, serviceDescription, servicePrice } =
+    service;
+  // const {user} = useContext(AuthContext);
+  return (
+    <div className="pl-40">
+      <div className="card flex flex-row bg-base-100 shadow-xl">
+        <div className="flex flex-col">
+
+        <figure>
+          <img className="w-96 h-80"
+            src={serviceImage}
+            alt="Movie"
+          />
+        </figure>
+        {serviceName}
         </div>
-    );
+        <div className="card-body pt-20 pr-40">
+        
+          <div className="card-actions justify-center">
+            <button className="btn w-36 btn-success">Edit</button>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn w-36 btn-success">Delete</button>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ManageServiceCard;
