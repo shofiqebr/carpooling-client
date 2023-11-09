343254aded35483bd23970f81d56e681aa8a4534
shofiq <shofiqebr@gmail.com>
2023-11-09 15:54:55 +0600

    added delete option

--- src/provider/components/ManageServiceCard.jsx
+++ src/provider/components/ManageServiceCard.jsx
@@ -1,11 +1,49 @@
 // import { useContext } from 'react';
 
+import Swal from "sweetalert2";
+
 // import { AuthContext } from '../AuthProvider';
 
-const ManageServiceCard = ({ service }) => {
+const ManageServiceCard = ({ service,services,setServices }) => {
   const { _id, serviceImage, serviceName, serviceDescription, servicePrice } =
     service;
   // const {user} = useContext(AuthContext);
+
+  const handleDelete = _id => {
+    console.log(_id);
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+
+            fetch(`https://carpooling-and-ride-sharing-service-server.vercel.app/services/${_id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your Coffee has been deleted.',
+                            'success'
+                        )
+                        const remaining = services.filter(cof => cof._id !== _id);
+                        setServices(remaining);
+                    }
+                })
+
+        }
+    })
+}
+
   return (
     <div className="pl-40">
       <div className="card flex flex-row bg-base-100 shadow-xl">
@@ -25,7 +63,7 @@ const ManageServiceCard = ({ service }) => {
             <button className="btn w-36 btn-success">Edit</button>
           </div>
           <div className="card-actions justify-center">
-            <button className="btn w-36 btn-success">Delete</button>
+            <button onClick={handleDelete} className="btn w-36 btn-success">Delete</button>
           </div>
          
         </div>
